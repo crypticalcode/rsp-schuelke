3fda729949a8379a133e51e74d5830e257e2ca20
Julian <julian.becker@isb-ik.de>
2021-05-26 00:40:52 +0200

    rsp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './company/company.component';
+import { LegalComponent } from './legal/legal.component';
+import { ProductsComponent } from './products/products.component';
+import { ServiceComponent } from './service/service.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CertificationComponent } from './certification/certification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'company', component: CompanyComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'certification', component: CertificationComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'legal', component: LegalComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
